9eaf25e164280bb206272df99c6a1b47c00d240a
Cilven <Minhtqce170364@fpt.edu.vn>
2025-03-06 14:01:57 +0700

    mm

--- src/routes/login.js
+++ src/routes/login.js
@@ -8,5 +8,8 @@ router.post('/register', loginControllers.register);
 router.post('/login', loginControllers.login);
 router.get('/logout', loginControllers.logout);
 // router.get('/trash/courses', loginControllers.trash);
+router.get('/register', (req, res) => {
+    res.render('account/register'); // Hiển thị trang đăng ký
+});
 
 module.exports = router;
